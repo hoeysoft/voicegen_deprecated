672e307d7bbfbe533e663de167e1d13fabbca601
hoey <nznial@gmail.com>
2015-07-27 23:13:45 +0900

    minor fix

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import os
+import clipboard
 from HTMLParser import HTMLParser
 from gtts import gTTS
 
 htmlp = HTMLParser()
 
 
-def gen_voice(sentence):
+def to_filename(sentence):
     underscored = ''.join(map(lambda x: x if x.isalnum() else '_', sentence))
-    filename = underscored + '.mp3'
+    return underscored + '.mp3'
+
+
+def genvoice(sentence, filename):
     gTTS(sentence).save(filename)
-    return filename
 
 os.system('clear')
-print 'voicegen'
-
 while True:
-    sentence = raw_input('voicegen:').strip()
-    filename = gen_voice(sentence)
-    os.system('printf "[sound:{}]" | pbcopy'.format(filename))
+    sentence = raw_input('vg:').strip()
+    filename = to_filename(sentence)
+    toclipboard = '{}\n[sound:{}]'.format(sentence, filename)
+    clipboard.copy(toclipboard)
+    genvoice(sentence, filename)
     os.system('afplay {}'.format(filename))
